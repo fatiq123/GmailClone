5b0b49ffc771ffa990e1a6ade954adcd7bec9249
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-10 07:13:44 -0700

    AppBar designed

--- app/src/main/java/com/example/gmailclone/MainActivity.kt
+++ app/src/main/java/com/example/gmailclone/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.gmailclone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.gmailclone.components.HomeAppBar
 import com.example.gmailclone.ui.theme.GmailCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +27,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GmailApp()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GmailApp() {
+    Scaffold(topBar = { TopAppBar(title = { HomeAppBar() }) }) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GmailCloneTheme {
-        Greeting("Android")
+        GmailApp()
     }
 }
